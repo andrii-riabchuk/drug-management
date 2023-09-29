67f44afbd79bd3c461888ba5d8ea32abcee920c4
Andrii Riabchuk <andrii.riabchuk.engineer@gmail.com>
2023-09-29 19:07:57 +0300

    Added basic layout, trying out Dart features

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,59 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random awesome idea:'),
+          BigCard(days: 30, message: "Sober Days"),
+          BigCard(days: 10, message: "Days To Party"),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.days,
+    required this.message,
+  });
+
+  final int days;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(children: [
+              Text(
+                "$days",
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(message)
+            ])));
   }
 }

eae362a0b46eef2e726025d3607030ef12e70074
Andrii Riabchuk <andrii.riabchuk.engineer@gmail.com>
2023-10-16 19:37:01 +0300

    [draft]: designed party page

--- lib/party.dart
+++ lib/party.dart
@@ -1,4 +1,6 @@
+import 'package:drug_management/shared_pref.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PartyPage extends StatelessWidget {
   const PartyPage({super.key});
@@ -6,18 +8,44 @@ class PartyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: const Text("Welcome to the party")),
         body: Padding(
-            padding: EdgeInsets.all(0),
+            padding: EdgeInsets.only(top: 20),
             child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    // mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text('Welcome to the party'),
+                      const SizedBox(
+                          width: 300.0,
+                          child: TextField(
+                              decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'What will you use?',
+                          ))),
                       // Text(
                       //     "Glad to see that you handled the 90-day-sobriety challenge")
+                      Padding(
+                          padding: EdgeInsets.all(10),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              MySharedPreferences.instance.setString(
+                                  "lastUseDate",
+                                  DateFormat('yyyy-MM-dd')
+                                      .format(DateTime.now()));
+                              Navigator.pushNamedAndRemoveUntil(
+                                  context, "/", (route) => false);
+                            },
+                            child: Text(
+                              'USEEE',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.all<EdgeInsets>(
+                                    EdgeInsets.all(10))),
+                          ))
                     ]))));
   }
 }

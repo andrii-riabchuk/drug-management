cdc4ca035bd1bf65f02bdc98d47d3adb6849f7e4
Andrii Riabchuk <andrii.riabchuk.engineer@gmail.com>
2023-11-21 23:57:17 +0200

    removed shared-preferences dependency

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:drug_management/main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    SharedPreferences prefs = await SharedPreferences.getInstance();
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
